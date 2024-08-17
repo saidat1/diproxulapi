333a044763b66009ae3558843cfdf34a424bd8f9
MUKE-coder <gmukejohnbaptist@gmail.com>
2024-08-17 17:22:13 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import customerRouter from "./routes/customer";
+
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`); // Log a message indicating the server is running
+});
+
+app.use("/api/v1", customerRouter);
